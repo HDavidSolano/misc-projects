a2c369b51ae23f251695424be1c2c763e0b87556
David Solano <hd.solano@gmail.com>
2018-07-09 17:51:23 -0400

    Added and modified the code
    added the modified

--- streamerMaster/simpleClient.py
+++ streamerMaster/simpleClient.py
@@ -3,26 +3,38 @@ import socket
 import struct        
 import cv2
 import numpy as np
+from time import sleep
 # Create a socket object
 s = socket.socket()         
  
 # Define the port on which you want to connect
-port = 5005   
+port = 5005  
 s.connect(('127.0.0.1', port))            
 while True:
 # connect to the server on local computer
     # receive data from the server
     len_str = s.recv(4)
     size = struct.unpack('!i', len_str)[0]
+    if size > 0:
+        data = s.recv(size)
+        
+        nparr = np.fromstring(data, np.uint8)
+        
+        img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+        
+        cv2.imshow('received',img)
+        #s.flush()
+        
+        gray_frame = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY) 
+        
+        data = cv2.imencode('.jpg', gray_frame)[1].tostring()
     
-    #print('size:', size)
+        len_str = struct.pack('!i', len(data))
+        sleep(0.1)
+        s.send(len_str)
     
-    data = s.recv(size)
+        s.send(data)
     
-    nparr = np.fromstring(data, np.uint8)
-    
-    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-    cv2.imshow('received',img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 cv2.waitKey(0)
--- streamerMaster/simpleServer.py
+++ streamerMaster/simpleServer.py
@@ -4,11 +4,12 @@ from time import sleep
 import signal
 import cv2
 import struct
+import numpy as np
 from CamRunnable import camVideoStream
 
-cam_holder = camVideoStream(0,30,640,480)
+#cam_holder = camVideoStream(0,30,320,240)
 
-cam_holder.start()
+#cam_holder.start()
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -18,13 +19,36 @@ server_socket.listen(5)
 c, addr = server_socket.accept()  
 print ('Got connection from', addr) 
 while True:
-    my_frame, t = cam_holder.read()
+    #my_frame, t = cam_holder.read()
+    my_frame = cv2.imread('newplot.png')
     data = cv2.imencode('.jpg', my_frame)[1].tostring()
     #print('len:', len(data))
     len_str = struct.pack('!i', len(data))
+    sleep(0.1)
     # send first the size of the image
     c.send(len_str)
-    # send the encoded image
+    # send the encoded image  
     c.send(data)
+    sleep(0.1)
+    len_str = c.recv(4)
+    
+    size = struct.unpack('!i', len_str)[0]
+    #print('size:', size)
+    if size > 0:
+
+        data = c.recv(size)
+    
+        nparr = np.fromstring(data, np.uint8)
+        
+        img = cv2.imdecode(nparr, cv2.IMREAD_GRAYSCALE)
+        
+        cv2.imshow('received Server',img)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 # Close the connection with the client
 c.close()
+
+cv2.waitKey(0)
+# close the connection
+cv2.destroyAllWindows()
