d0d2091de2a75a73a29fbd9add84481549af03bd
david <david@david-System>
2018-08-05 19:05:34 -0400

    Agregue la capacidad de transmitir sonido via socket, experimental

--- streamerMaster/advancedServer.py
+++ streamerMaster/advancedServer.py
@@ -16,13 +16,13 @@ cam_holder.start()
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server_socket.bind(('localhost', 5005))
+server_socket.bind(('192.168.10.106', 5005))
 server_socket.listen(5)
 
-num_users = 1 # this specifies the number of users connected to the server
+num_users = 2# this specifies the number of users connected to the server
 
 connected_users = 0
-time_delay = 0.25
+time_delay = 0.05
 while connected_users  < num_users:
     c, addr = server_socket.accept()
     clients.append(c)
@@ -30,7 +30,6 @@ while connected_users  < num_users:
     connected_users += 1
 sleep(4)
 while True:
-    #my_frame = cv2.imread('newplot.png')
     my_frame, t = cam_holder.read()
     # First I prepare the image to be sent along with its size:
     data = cv2.imencode('.jpg', my_frame)[1].tostring()
@@ -42,28 +41,4 @@ while True:
         # send the encoded image  
         a_client.send(data)
     sleep(time_delay)
-"""
-async def handle_client(client):
-    request = None
-    while request != 'quit':
-        request = (await loop.sock_recv(client, 1024)).decode('utf8')
-        response = request + ' me llego'
-        await loop.sock_sendall(client, response.encode('utf8'))
-    client.close()
-
-async def run_server():
-    while True:
-        client, _ = await loop.sock_accept(server)
-        loop.create_task(handle_client(client))
-
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(('localhost', 5005))
-server.listen(8)
-server.setblocking(False)
-
-loop = asyncio.get_event_loop()
-try:
-    loop.run_until_complete(run_server())
-except KeyboardInterrupt:
-    server.close() """
 
--- streamerMaster/simpleServer.py
+++ streamerMaster/simpleServer.py
@@ -1,30 +1,62 @@
 import socket
-import _thread
 from time import sleep
-import signal
 import cv2
 import struct
+import numpy as np
+import math as m
 from CamRunnable import camVideoStream
 
-cam_holder = camVideoStream(0,30,640,480)
+clients=[]
+
+
+cam_holder = camVideoStream(0,30,320,240)
 
 cam_holder.start()
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server_socket.bind(('localhost', 5005))
+server_socket.bind(('192.168.10.106', 5005))
 server_socket.listen(5)
-# Establish connection with client.
-c, addr = server_socket.accept()  
-print ('Got connection from', addr) 
+
+num_users = 1# this specifies the number of users connected to the server
+
+connected_users = 0
+time_delay = 0.05
+while connected_users  < num_users:
+    c, addr = server_socket.accept()
+    clients.append(c)
+    print ('Got connection from', addr) 
+    connected_users += 1
+sleep(4)
 while True:
     my_frame, t = cam_holder.read()
+    # First I prepare the image to be sent along with its size:
     data = cv2.imencode('.jpg', my_frame)[1].tostring()
-    #print('len:', len(data))
     len_str = struct.pack('!i', len(data))
-    # send first the size of the image
-    c.send(len_str)
-    # send the encoded image
-    c.send(data)
-# Close the connection with the client
-c.close()
+    #print("sending...")
+    cli_count = 1
+    for a_client in clients:
+        # send first the size of the image
+        a_client.send(len_str)
+        # send the encoded image  
+        a_client.send(data)
+        
+        sleep(0.05)
+        # now receive from the client a modified file to watch
+        len_str = a_client.recv(4)
+       
+        size = struct.unpack('!i', len_str)[0]
+        
+        if size > 0:
+    
+            data2 = a_client.recv(size)
+       
+            nparr = np.fromstring(data2, np.uint8)
+            if m.fabs(size) < 50000:
+                img = cv2.imdecode(nparr, cv2.IMREAD_GRAYSCALE)
+                cv2.imshow('received Server ' + str(cli_count),img)
+        cli_count += 1
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        
+    sleep(time_delay)
